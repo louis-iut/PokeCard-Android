44230e2c821617ec9abc6af9664e78839e6faf7c
Quentin G <quentin01960@orange.fr>
2017-11-15 11:14:54 +0100

    Added facebook login button

--- app/src/main/java/com/example/louis/pokecard_android/presentation/activity/LoginActivity.java
+++ app/src/main/java/com/example/louis/pokecard_android/presentation/activity/LoginActivity.java
@@ -1,15 +1,56 @@
 package com.example.louis.pokecard_android.presentation.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.example.louis.pokecard_android.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    CallbackManager callbackManager = CallbackManager.Factory.create();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        //Init Facebook SDK
         setContentView(R.layout.activity_login);
+
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_facebook_button);
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println("onSuccess");
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+
+            }
+
+        });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,
+                resultCode, data);
+    }
+
 }
